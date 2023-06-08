941d48ac531894682b28eb53b7d8cd2b9ed0feb2
Sasa Petalinkar <sasa5linkar@gmail.com>
2023-06-09 00:43:15 +0200

    SVM done

--- train_svm.py
+++ train_svm.py
@@ -11,18 +11,22 @@ from sklearn.feature_extraction.text import TfidfTransformer
 import pandas as pd
 from copy import deepcopy
 from sklearn.metrics import classification_report
+from joblib import dump
+from sklearn.metrics import confusion_matrix
 
 #Constansts
 TRAIN_DIR = ".//train_sets//"
 RES_DIR = ".\\resources\\"
-REP_DIR = ".\\reports\\"
-
+REP_DIR = ".\\reports\\SVM\\"
+MOD_DIR = ".\\ml_models\\"
 
 def load_file_into_list(filename):
     with open(filename, mode="r", encoding="utf-16") as file:
         lines = [line.strip() for line in file]
     return lines
-def train_pipe(pipe, name):
+
+
+def train_pipe(pipe, i, polarity):
   """Trains a pipeline on data from a given polarity set.
 
   Args:
@@ -32,20 +36,20 @@ def train_pipe(pipe, name):
   Returns:
     The trained pipeline.
   """
-
+  name = "LM" + polarity + str(i) + ".csv"
   # Read the data from the CSV file.
   X = pd.read_csv(TRAIN_DIR + "X_train_" + name)["Sysnet"]
-  y = pd.read_csv(TRAIN_DIR + "y_train_" + name)["POS"]
-
-  # Clone the pipeline using the deepcopy function.
+  y = pd.read_csv(TRAIN_DIR + "y_train_" + name)[polarity]
   ret_pipe = deepcopy(pipe)
-
+  ret_pipe.set_params(**best_parameters[(polarity,i)])
   # Fit the pipeline to the data.
   ret_pipe.fit(X, y)
 
   # Return the trained pipeline.
   return ret_pipe
-def test_pipe(pipe, name):
+
+
+def test_pipe(pipe, name, polarity):
   """Test trained pipeline on data from a given polarity set.
       Saves reports in REP_DIR folder under report_(name).txt
       Report contains gerated clasifcation report
@@ -63,24 +67,28 @@ def test_pipe(pipe, name):
 
   # Read the data from the CSV file.
   X = pd.read_csv(TRAIN_DIR + "X_test_" + name)["Sysnet"]
-  y = pd.read_csv(TRAIN_DIR + "y_test_" + name)["POS"]
+  y = pd.read_csv(TRAIN_DIR + "y_test_" + name)[polarity]
   # Create a variable for the predicted values.
   y_predicted = pipe.predict(X)
-  
-  # Create a classification report.
-  report = classification_report(y, y_predicted)
 
+  # Create a classification report.
+  report = classification_report(y, y_predicted, target_names= ["OBJ", 
+                                                                polarity])
+  con_mat = confusion_matrix(y, y_predicted)
   # Save the report to a file.
   with open(REP_DIR + "report_" + name + ".txt", "w") as f:
-    f.write(report)
+      f.write(str(con_mat))
+      f.write("\n\n")
+      f.write(report)
   # Create a table of misclassified X values.
   table = pd.DataFrame({"X": X, "Predicted": pipe.predict(X), "Real": y})
   # Create a table of misclassified X values.
   misclassified_X = table[table["Predicted"] != table["Real"]]
 
   # Save the table to a file.
-  misclassified_X.to_csv(REP_DIR + "table_" + name, index=False)    
+  misclassified_X.to_csv(REP_DIR + "table_" + name, index=False)
   return None
+
 
 stop_words3 = load_file_into_list(RES_DIR + "stopwordsSRB tf.txt")
 stop_words3 = stop_words3 + ['daljem', 'izgledalo', 'izgledu', 'međuvremenu',
@@ -104,14 +112,98 @@ pipe = Pipeline(
 
         ("svm", SVC()),
 
-        
+
     ],
     memory="F:\\Temp\\ML_Cache"
 )
 pipe.set_params(**PAR)
-pipes = {}
+best_parameters = {
+    ('POS', 0): {
+        'svm__C': 10000000000000.0,
+        'svm__cache_size': 1000,
+        'svm__class_weight': 'balanced',
+        'svm__gamma': 1e-09,
+        'svm__kernel': 'rbf',
+        'tfidf__use_idf': True,
+        "vect__stop_words": stop_words3,
+        'vect__ngram_range': (1, 1)
+    },
+    ('NEG', 0): {
+        'svm__C': 10000000.0,
+        'svm__cache_size': 1000,
+        'svm__class_weight': 'balanced',
+        'svm__gamma': 1e-07,
+        'svm__kernel': 'rbf',
+        'tfidf__use_idf': True,
+        "vect__stop_words": stop_words3,
+        'vect__ngram_range': (1, 1)
+    },
+    ('POS', 2): {
+        'svm__C': 10000000.0,
+        'svm__cache_size': 1000,
+        'svm__class_weight': 'balanced',
+        'svm__gamma': 1e-08,
+        'svm__kernel': 'rbf',
+        'tfidf__use_idf': True,
+        "vect__stop_words": stop_words3,
+        'vect__ngram_range': (1, 1)
+    },
+    ('NEG', 2): {
+        'svm__C': 10000000.0,
+        'svm__cache_size': 1000,
+        'svm__class_weight': 'balanced',
+        'svm__gamma': 1e-08,
+        'svm__kernel': 'rbf',
+        'tfidf__use_idf': True,
+        "vect__stop_words": stop_words3,
+        'vect__ngram_range': (1, 1)
+    },
+    ('POS', 4): {
+        'svm__C': 10000000.0,
+        'svm__cache_size': 1000,
+        'svm__class_weight': 'balanced',
+        'svm__gamma': 1e-08,
+        'svm__kernel': 'rbf',
+        'tfidf__use_idf': True,
+        "vect__stop_words": stop_words3,
+        'vect__ngram_range': (1, 1)
+    },
+    ('NEG', 4): {
+        'svm__C': 10000000000.0,
+        'svm__cache_size': 1000,
+        'svm__class_weight': 'balanced',
+        'svm__gamma': 1e-09,
+        'svm__kernel': 'rbf',
+        'tfidf__use_idf': True,
+        "vect__stop_words": stop_words3,
+        'vect__ngram_range': (1, 1)
+    },
+    ('POS', 6): {
+        'svm__C': 10000000.0,
+        'svm__cache_size': 1000,
+        'svm__class_weight': 'balanced',
+        'svm__gamma': 1e-08,
+        'svm__kernel': 'rbf',
+        'tfidf__use_idf': True,
+        "vect__stop_words": stop_words3,
+        'vect__ngram_range': (1, 1)
+    },
+    ('NEG', 6): {
+        'svm__C': 10000000000000.0,
+        'svm__cache_size': 1000,
+        'svm__class_weight': 'balanced',
+        'svm__gamma': 1e-08,
+        'svm__kernel': 'rbf',
+        'tfidf__use_idf': True,
+        "vect__stop_words": stop_words3,
+        'vect__ngram_range': (1, 1)
+    },
+}
+
 for i in range(0, 7, 2):
   for polarity in ["POS", "NEG"]:
-      name = "LM"+ polarity + str(i) + ".csv"
-      key = polarity + str(i)
-      print(name)
+      name = "LM" + polarity + str(i) + ".csv"
+      pipe1 = train_pipe(pipe, i, polarity)
+      test_pipe(pipe1, name, polarity)
+      filename = "SVM-klas-" + polarity +"-" + str(i) + ".joblit"
+      dump(pipe1,MOD_DIR+filename)
